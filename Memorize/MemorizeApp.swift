7b6f3fdbc53354b36a341ce2add8566a7c324e7c
RoryNaught <141364481+RoryNaught@users.noreply.github.com>
2024-08-23 10:48:26 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Rory Naughton on 8/23/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

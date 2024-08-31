5a6f156c321331a4a06321995f697d61d6bbc4d6
RoryNaught <141364481+RoryNaught@users.noreply.github.com>
2024-08-30 22:54:39 -0400

    a lot

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,45 +7,75 @@
 
 import SwiftUI
 
-struct ContentView: View 
-{
-	let emojis : Array<String> = ["👹","🎃","👻","💀","🎃"]
+struct ContentView: View {
+	let emojis : Array<String> = ["👹","🎃","👻","💀","👺","🕷️","🕸️","😱","🙀","☠️","🍬","😈"]
 	
-    var body: some View
-    {
-		HStack
-		{
-			ForEach(emojis.indices, id: \.self) { index in
-				cardView(content: emojis[index])
+	@State var cardCount: Int = 4
+	
+	var body: some View {
+		VStack {
+			ScrollView {
+				cards
 			}
+			Spacer()
+			cardCountAdjusters
 		}
-		.foregroundColor(.orange)
 		.padding()
-    }
+	}
+	
+	var cardCountAdjusters: some View {
+		HStack {
+			cardRemover
+			Spacer()
+			cardAdder
+		}
+		.imageScale(.large)
+		.font(.largeTitle)
+	}
+	
+	func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+		Button(action: {
+			cardCount += offset
+		}, label: {
+			Image(systemName: symbol)
+		})
+		.disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+	}
+	
+	var cards: some View {
+		LazyVGrid( columns: [GridItem(.adaptive(minimum: 120))] )  {
+			ForEach(0..<cardCount, id: \.self) { index in
+				cardView(content: emojis[index]).aspectRatio(2/3, contentMode: .fit)
+			}
+		}.foregroundColor(.orange)
+	}
+	
+	var cardRemover: some View {
+		return cardCountAdjuster(by: -1, symbol:"rectangle.stack.badge.minus.fill")
+	}
+	
+	var cardAdder: some View {
+		return cardCountAdjuster(by: 1, symbol:"rectangle.stack.badge.plus.fill")
+	}
+	
 }
 
-struct cardView: View 
-{
+struct cardView: View {
 	let content: String
 	@State var isFaceUp = true
 	
-	var body: some View
-	{
-		ZStack
-		{
+	var body: some View {
+		ZStack {
 			let base = RoundedRectangle(cornerRadius:13)
-			if isFaceUp
-			{
+			Group {
 				base.foregroundColor(.white)
 				base.strokeBorder(lineWidth: 2)
 				Text(content).font(.largeTitle)
-			} else
-			{
-				RoundedRectangle(cornerRadius:13)
 			}
+			.opacity(isFaceUp ? 1 : 0)
+			base.fill().opacity(isFaceUp ? 0 : 1)
 		}
-		.onTapGesture
-		{
+		.onTapGesture {
 			isFaceUp.toggle()
 		}
 	}
